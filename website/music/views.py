5af228c8329c3bdbedcbc359ed7625c8659c0d19
ridooo <ridooprent@gmail.com>
2018-01-24 10:21:52 +0700

    add: bg image

--- website/music/views.py
+++ website/music/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import SongForm
+from .models import Song
 
 def index(request):
     if request.method == 'POST':
         form = SongForm(request.POST)
         if form.is_valid():
-            # save the data here
+            form.save()
             return HttpResponseRedirect('/')
 
     else:
         form = SongForm()
-    return render(request, 'music/index.html', {'form': form})
+    return render(request, 'music/index.html', {'form': form, 'songs': Song.objects.all() })
