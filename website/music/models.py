e41884c6b0fb578ffbbe3f29637e9fc6ed91d4f7
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-01-28 09:27:09 +0700

    change add.html to song_form.html, change save btn's color etc

--- website/music/models.py
+++ website/music/models.py
@@ -14,7 +14,7 @@ class Song(models.Model):
     title = models.CharField(max_length=50)
     genre = models.CharField(max_length=25, choices=genre_choices, default='Pop')
     singer = models.CharField(max_length=50)
-    rating = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField(default=1)
 
     def __unicode__(self):
         return self.title
--- website/music/urls.py
+++ website/music/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"), 
-    url(r'^edit/(?P<pk>\d+)$', views.edit, name='edit_song'),
+    url(r'^edit/(?P<pk>\d+)$', views.edit, name='edit'),
     url(r'^delete/(?P<pk>\d+)$', views.delete, name='delete'), 
 ]
