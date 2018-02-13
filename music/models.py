831a83728dd4734d32507fe67932dadd49427581
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-02-13 07:18:28 +0700

    fix profile model

--- music/models.py
+++ music/models.py
@@ -36,10 +36,7 @@ class Profile(models.Model):
         return self.user 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
